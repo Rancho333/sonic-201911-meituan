c12cd17a10191aa41c16a3abfa7434eaf50aff46
Rancho <razhang@celestica.com>
2021-01-07 15:31:36 +0800

    clean platform, only save broadcom

--- platform/barefoot/bfn-platform.mk
+++
@@ -1,5 +0,0 @@
-BFN_PLATFORM = bfnplatform_20201023_sai_1.5.2_deb9.deb
-$(BFN_PLATFORM)_URL = "https://github.com/barefootnetworks/sonic-release-pkgs/raw/dev/$(BFN_PLATFORM)"
-
-SONIC_ONLINE_DEBS += $(BFN_PLATFORM)
-$(BFN_SAI_DEV)_DEPENDS += $(BFN_PLATFORM)
--- platform/innovium/sonic-platform-modules-delta/evs-a-32q56/scripts/evs-a-32q56_platform_init.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-#platform init script for Delta EVS-A-32Q56
-
-
-
-exit 0
-
-
